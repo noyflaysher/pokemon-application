1cc9477cc95c75bdcb8ecd068e733b35ec36dd6e
Noy Flaysher <94534918+noyflaysher@users.noreply.github.com>
2023-01-10 12:52:16 +0200

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,18 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   userEmail: string;
   userApprove: string = 'demo@skills.co.il';
+  wrongEmail: boolean = false;
 
   constructor(private router: Router) {}
 
   loginUser() {
     if (this.userEmail === this.userApprove) {
+      let data = { isLogIn: true };
+      localStorage.setItem('isLogIn', JSON.stringify(data));
       this.router.navigate(['/home']);
       console.log(`log in`);
+    } else {
+      this.wrongEmail = true;
     }
   }
 }
